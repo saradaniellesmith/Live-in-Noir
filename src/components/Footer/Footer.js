f87fc91674fda0c8bcc57ad483403f9f00911f62
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-26 10:21:28 -0600

    server running

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            
+            <p> Heyyyy there </p> 
+
+        )
+    }
+}
+export default Footer;
