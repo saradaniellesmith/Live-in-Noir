c5bebfd1171e5cbaa9a8c60a06feb12f02899b68
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-26 18:32:49 -0600

    added social media icons to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,20 @@ class Footer extends Component {
     render() {
         return (
     <div className="footer">
-        <div className="footer-left">
-        
-            <Link to="/about"> 
-                <span> About Us </span>
-            </Link>
-            <Link to="/contact">
-                <span> Contact </span>
-            </Link>
-            <Link to="/returnpolicy">
-                <span> Return Policy </span>
-            </Link>
 
+        <div className="footer-left">
+            <Link to="/about"> <span> About Us </span> </Link>
+            <Link to="/contact"> <span> Contact </span> </Link>
+            <Link to="/returnpolicy"> <span> Return Policy </span> </Link>
         </div>
 
-        <div className="footer-center">
+        <div className="footer-social-icons">
+            <h4 className="follow"> Stay Connected </h4>
+            <ul className="social-icons">
+                <li><a href="" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
+                <li><a href="" className="social-icon"> <i className="fa fa-twitter"></i></a></li>
+                <li><a href="" className="social-icon"> <i className="fa fa-instagram"></i></a></li>
+            </ul>
         </div>
 
         <div className="footer-right">
