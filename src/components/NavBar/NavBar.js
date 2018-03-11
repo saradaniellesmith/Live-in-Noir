7cf7b41344726e5837b522fe67f93b6112a2f076
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-03-10 18:17:16 -0600

    added more photos to product details page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,8 +22,8 @@ class NavBar extends Component {
                 <p> <Link to="/" style={{ textDecoration: 'none', color: 'black' }}> HOME </Link> </p>
                 <p> <Link to="/shop/:shop" style={{ textDecoration: 'none', color: 'black' }}> SHOP </Link> </p>
                 {/* <p> <Link to="/about"> ABOUT </Link> </p> */}
-                <p> <a href={process.env.REACT_APP_LOGIN} style={{ textDecoration: 'none', color: 'black' }}> LOGIN </a> </p>
                 <p> <Link to="/shoppingcart" style={{ textDecoration: 'none', color: 'black' }}> <img src={require('./bag.png')} style={{ height: '3.2vh', width: 'auto'}}/> </Link> </p>
+                <p> <a href={process.env.REACT_APP_LOGIN} style={{ textDecoration: 'none', color: 'black' }}> LOGIN </a> </p>
             </div>
         </div>
     </div>
